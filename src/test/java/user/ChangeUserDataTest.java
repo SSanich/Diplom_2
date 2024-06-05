8d89024e29b20ba4321992357cd34085dc772198
a.grigoriev <a.grigoriev@anketolog.ru>
2024-06-05 14:26:36 +0700

    edit name class ChangeUserData

--- src/test/java/user/ChangeUserDataTest.java
+++ src/test/java/user/ChangeUserDataTest.java
@@ -11,7 +11,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class ChangeUserWithAuthTest {
+public class ChangeUserDataTest {
     private String accessToken;
     User user;
     private final UserClientAPI userClientAPI = new UserClientAPI();
