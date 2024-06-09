f72ba20c8b16f9dcf42b7f20bf5153fe13c485be
Aleksandr Grigoriev <sanich.grig@gmail.com>
2024-06-09 08:30:31 +0700

    add spec to Ingredients API

--- src/main/java/praktikum/ingredients/IngredientsAPI.java
+++ src/main/java/praktikum/ingredients/IngredientsAPI.java
@@ -3,16 +3,15 @@ package praktikum.ingredients;
 import praktikum.Constants;
 import io.qameta.allure.Step;
 import io.restassured.http.ContentType;
+import praktikum.Model;
 
 import static praktikum.Constants.BASE_URI;
 import static io.restassured.RestAssured.given;
 
-public class IngredientsAPI {
+public class IngredientsAPI   extends Model {
         @Step("Получение списка ингредиентов")
         public OrderIngredients getIngredients() {
-            return given()
-                    .contentType(ContentType.JSON)
-                    .baseUri(BASE_URI)
+            return spec()
                     .get(Constants.INGREDIENTS_URI)
                     .then().log().all()
                     .extract()
